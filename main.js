2105cf81ba3ff3838b5aae15deaf2630b636483e
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-08-22 20:40:45 +0100

    Implement function to reduce rawData to a small object

--- main.js
+++ main.js
@@ -4,10 +4,31 @@ async function getDataFromWeatherApi(location) {
 			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current&key=5CBXU7NTXLKWV97R9QQJSQ72T&contentType=json`,
 			{ mode: "cors" }
 		);
-		const dataJson = await res.json();
-		return dataJson;
+		const data = await res.json();
+		return data;
 	} catch (err) {
 		console.error(err);
 		return {};
 	}
+}
+
+function processRawWeatherData(rawData) {
+	try {
+		var weather = rawData.currentConditions;
+	} catch {
+		console.error("Tried to process invalid data! :");
+		console.error(rawData);
+		return {};
+	}
+
+	return {
+		condition: weather.conditions,
+		tempF: weather.temp,
+		tempC: ((weather.temp - 32) * 5) / 9, // fahrenheit to celsius
+		humidity: weather.humidity,
+		time: weather.datetime,
+		precipitation: weather.precip,
+		icon: weather.icon,
+		location: rawData.address,
+	};
 }
