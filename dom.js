f153ecc796094f08c2d02e85fcd699fad39f3e1b
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-08-23 21:24:12 +0100

    Change bg gradient based on temp

--- dom.js
+++ dom.js
@@ -12,10 +12,16 @@ const domHandler = (function () {
 		if (data == {}) return;
 
 		elements.temp.textContent = `Temperature: ${data.tempC}°C`;
-		elements.condition.textContent = data.condition;
+		elements.condition.textContent = data.condition + ".";
 		elements.humidity.textContent = `Humidity: ${data.humidity}%`;
 		elements.location.textContent = data.location;
 		elements.time.textContent = data.time.slice(0, -3); // returns the time without the last three chars (which are the seconds)
+
+		if (data.tempC > 40) elements.body.className = "very-hot";
+		else if (data.tempC > 32) elements.body.className = "hot";
+		else if (data.tempC > 22) elements.body.className = "nice";
+		else if (data.tempC > 12) elements.body.className = "cold";
+		else elements.body.className = "freezing";
 	}
 
 	return { render };
